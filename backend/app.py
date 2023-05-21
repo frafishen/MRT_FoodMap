2bc4603febf52b10441b3a2c372c4e5b6ae1812a
frafishen <frafishen@gmail.com>
2023-05-21 21:26:58 +0800

    frontend & backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,12 +9,6 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/example_endpoint', methods=['POST'])
-def example_endpoint():
-    data = request.get_json()
-    print(data)
-    # perform your operations with the data
-    return jsonify({'status': 'success'}), 200
 
 if __name__ == '__main__':
     app.run(port=5000)
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="getHelloWorld">Get Hello World</button>
+    <p>{{ helloWorldText }}</p>
+  </div>
+</template>
+
+<script>
+
+async function getHelloWorld () {
+  try {
+    const response = await fetch('http://localhost:5000/')
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    this.helloWorldText = await response.text()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      helloWorldText: ''
+    }
+  },
+  methods: {
+    getHelloWorld: getHelloWorld
+  }
+}
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
